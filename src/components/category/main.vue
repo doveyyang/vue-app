4914ed842ad3b87e66dddfac623352efe256059b
Dovey <172399463@qq.com>
2019-02-08 21:04:37 +0800

    增加分类页面

---
+++ src/components/category/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+
+    </div>
+</template>
+<script>
+import mainLeft from './mainLeft'
+import mainRight from './mainRight'
+export default {
+    data(){
+        return {
+
+        }
+    },
+    components:{
+        mainLeft,mainRight
+    }
+}
+</script>
